2db18117e6a589e882bf178bd43631b8fd2cd18e
Julio Castillo <julio.a.castillo.c@gmail.com>
2025-10-25 20:19:09 +0000

    feat: task 1, exclusive ownership 1

--- src/chatgui.cpp
+++ src/chatgui.cpp
@@ -130,7 +130,7 @@ ChatBotPanelDialog::ChatBotPanelDialog(wxWindow* parent, wxWindowID id)
     ////
 
     // create chat logic instance
-    _chatLogic = new ChatLogic();
+    _chatLogic = std::make_unique<ChatLogic>();
 
     // pass pointer to chatbot dialog so answers can be displayed in GUI
     _chatLogic->SetPanelDialogHandle(this);
@@ -146,7 +146,8 @@ ChatBotPanelDialog::~ChatBotPanelDialog() {
     //// STUDENT CODE
     ////
 
-    delete _chatLogic;
+    // No explicit cleanup needed:
+    // _chatLogic is a unique_ptr, automatically destroyed and deallocates ChatLogic
 
     ////
     //// EOF STUDENT CODE
